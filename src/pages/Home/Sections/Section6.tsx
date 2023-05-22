e2617e2d34507ff095db78d274766cefe994e0ab
Juphet Vitualla <jvitualla@dnamicro.com>
2023-05-22 10:19:32 +0800

    initial update post design

--- src/pages/Home/Sections/Section6.tsx
+++ src/pages/Home/Sections/Section6.tsx
@@ -1,139 +1,175 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import post1 from '../../../assets/images/posts/post1.jpg'
-import post2 from '../../../assets/images/posts/post2.jpg'
-import post3 from '../../../assets/images/posts/post3.jpg'
+import post1 from "../../../assets/images/posts/post1.jpg";
+import post2 from "../../../assets/images/posts/post2.jpg";
+import post3 from "../../../assets/images/posts/post3.jpg";
+import logo from "../../../assets/images/logo-small.png";
 
 const items = [
-    {
-        id: '1',
-        title: 'Heading 1',
-        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, id, sunt autem voluptatem nesciunt et inventore doloribus consequuntur ratione',
-        images: post1
-    },
-    {
-        id: '2',
-        title: 'Heading 2',
-        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, id, sunt autem voluptatem nesciunt et inventore doloribus consequuntur ratione',
-        images: post2
-    },
-    {
-        id: '3',
-        title: 'Heading 3',
-        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, id, sunt autem voluptatem nesciunt et inventore doloribus consequuntur ratione',
-        images: post3
-    },
-    {
-        id: '4',
-        title: 'Heading 4',
-        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, id, sunt autem voluptatem nesciunt et inventore doloribus consequuntur ratione',
-        images: post1
-    },
-    {
-        id: '5',
-        title: 'Heading 5',
-        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, id, sunt autem voluptatem nesciunt et inventore doloribus consequuntur ratione',
-        images: post2
-    },
-    {
-        id: '6',
-        title: 'Heading 6',
-        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, id, sunt autem voluptatem nesciunt et inventore doloribus consequuntur ratione',
-        images: post3
-    }
-]
+  {
+    id: "1",
+    title: "Heading 1",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, id, sunt autem voluptatem nesciunt et inventore doloribus consequuntur ratione",
+    images: post1,
+  },
+  {
+    id: "2",
+    title: "Heading 2",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, id, sunt autem voluptatem nesciunt et inventore doloribus consequuntur ratione",
+    images: post2,
+  },
+  {
+    id: "3",
+    title: "Heading 3",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, id, sunt autem voluptatem nesciunt et inventore doloribus consequuntur ratione",
+    images: post3,
+  },
+  {
+    id: "4",
+    title: "Heading 4",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, id, sunt autem voluptatem nesciunt et inventore doloribus consequuntur ratione",
+    images: post1,
+  },
+  {
+    id: "5",
+    title: "Heading 5",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, id, sunt autem voluptatem nesciunt et inventore doloribus consequuntur ratione",
+    images: post2,
+  },
+  {
+    id: "6",
+    title: "Heading 6",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, id, sunt autem voluptatem nesciunt et inventore doloribus consequuntur ratione",
+    images: post3,
+  },
+];
 
-interface StyledProps{
-    image: any
+interface StyledProps {
+  image: any;
 }
 
 function Section6() {
-
   return (
-    <StyledCont className='section Section6'>
-        <div className="wrapper">
-            <div className="sec6_cont">
-                <h2>See our Latest Posts</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, id, sunt autem voluptatem nesciunt et inventore doloribus consequuntur ratione</p>
-                <div className="posts_cont">
-                    {
-                        items.map(item => {
-                            const {id, title, content, images} = item
-                            return (
-                                <PostItem key={id} className='post_item' image={images}>
-                                    <div className='post_content'>
-                                        <h3>{title}</h3>
-                                        <p>{content}</p>
-                                    </div>
-                                </PostItem>
-                            )
-                        })
-                    }
+    <StyledCont className="section Section6">
+      <div className="wrapper">
+        <div className="sec6_cont">
+          <h2>See our Latest Posts</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Repellendus, id, sunt autem voluptatem nesciunt et inventore
+            doloribus consequuntur ratione
+          </p>
+          <div className="posts_cont">
+            {items.map((item) => {
+              const { id, title, content, images } = item;
+              return (
+                <div key={id} className="post_item max-w-[360px] bg-white ">
+                  <div className="post_content">
+                    <figure className="relative">
+                      <img
+                        alt="images"
+                        src={images}
+                        className="block w-full  h-[200px] object-cover"
+                      />
+                      <div className=" absolute right-0 bottom-0 triangle">
+                        <img
+                          src={logo}
+                          className="w-[45px] h-[45px] max-w-[unset] absolute right-3 bottom-[-5.5rem]"
+                          alt="logo"
+                        />
+                      </div>
+                    </figure>
+                    <div className="p-4 border border-[#14adc1] border-t-0">
+                      <h3 className="text-[25px] mb-2">{title}</h3>
+                      <p className="mb-4 text-gray-600 font-thin">{content}</p>
+                      <a
+                        href=""
+                        className="min-w-[6rem] inline-block border border-gray-200 text-sm text-center p-2 rounded-full mt-4"
+                      >
+                        Read More
+                      </a>
+                    </div>
+                  </div>
                 </div>
-            </div>
+              );
+            })}
+          </div>
         </div>
+      </div>
     </StyledCont>
-  )
+  );
 }
 
 const StyledCont = styled.div`
-    background: #219bc5;
-    padding: 4rem 0;
-    min-height: 400px;
-    h2{
-        color: #fff;
-        font-size: 2rem;
-        margin-bottom: 1rem;
-    }
-    p{
-        color: #fff;
-    }
-    .posts_cont{
-        display: flex;
-        justify-content: center;
-        gap: 1.5rem;
-        margin-top: 4rem;
-        flex-wrap: wrap;        
-    }
-`
+  background: #219bc5;
+  padding: 4rem 0;
+  min-height: 400px;
+
+  .triangle {
+    display: inline-block;
+    width: 0;
+    height: 0;
+    border-style: solid;
+    border-width: 0 0 100px 124px;
+    border-color: transparent transparent rgba(44, 45, 45, 0.6) transparent;
+  }
+  h2 {
+    color: #fff;
+    font-size: 2rem;
+    margin-bottom: 1rem;
+  }
+  .posts_cont {
+    display: flex;
+    justify-content: center;
+    gap: 1.5rem;
+    margin-top: 4rem;
+    flex-wrap: wrap;
+  }
+`;
 
 const PostItem = styled.div<StyledProps>`
-    &.post_item{
-        cursor: pointer;
-        width: 30%;
-        background: url(${(par => par.image) }) no-repeat center center;
-        min-height: 300px;
-        background-color: gray;
-        transition: all ease-in-out 300ms;
-        position: relative;
-        padding: 1rem;
-        &:before{
-            content:"";
-            position: absolute;
-            width: 100%;
-            height: 100%;
-            background: rgba(0,0,0, .5);
-            overflow: hidden;
-            inset:0;
-        }
-        &:hover{
-            opacity: .6;
-        }
-        .post_content{
-            position: absolute;
-            z-index: 1;
-            bottom:1.5rem;
-            h3{
-                color: #fff;
-                margin-bottom: 1rem;
-            }
-            p{
-                color: #fff;
-                font-size: 13px;
-            }
-        }        
+  &.post_item {
+    cursor: pointer;
+    width: 30%;
+    background: url(${(par) => par.image}) no-repeat center center;
+    min-height: 300px;
+    background-color: gray;
+    transition: all ease-in-out 300ms;
+    position: relative;
+    padding: 1rem;
+    &:before {
+      content: "";
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      background: rgba(0, 0, 0, 0.5);
+      overflow: hidden;
+      inset: 0;
     }
-`
+    &:hover {
+      opacity: 0.6;
+    }
+    .post_content {
+      position: absolute;
+      z-index: 1;
+      bottom: 1.5rem;
+      h3 {
+        color: #fff;
+        margin-bottom: 1rem;
+      }
+      p {
+        color: #fff;
+        font-size: 13px;
+      }
+    }
+  }
+`;
 
-export default Section6
+export default Section6;
